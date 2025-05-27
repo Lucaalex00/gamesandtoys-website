eb98cf71afbb31625c7729cbadb1420b7de95c54
Lucaalex00 <lu.al.cirio00@gmail.com>
2025-05-27 17:52:37 +0200

    first commit

---
+++ gamesandtoys-website/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">
+        Benvenuto nel Sito-Negozio
+      </h1>
+      <button className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-2 rounded">
+        Prova Tailwind
+      </button>
+    </div>
+  )
+}
+
+export default App
